a6804af164540b2985d229c240ba330f0fc0cb8d
tykayn du vortex a chats <tykayn@gmail.com>
2014-03-13 10:50:23 +0100

    clean console log

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -6,68 +6,64 @@ hexa = '0123456789ABCDEF'; // all colours
 
 //get a random colour
 rColor = function() {
-    
     var arr = hexa.split('');
     var color = '#';
     for (var i = 0; i < 3; i++) {
         color += arr[Math.floor(Math.random() * arr.length)];
     }
-    console.log(color);
     return color;
 }
 // manages the 4 sub blocks with their deepness.
 subBlocks = function(level) {
     var splittable = 1;
-    if(level >= levelMax){
+    if (level >= levelMax) {
         splittable = 0;
     }
-    var b ='';
+    var b = '';
     for (var i = 0; i < 4; i++) {
         var color = rColor();
         var style = 'background : ' + color;
-        var block = '  <div class="c block" style="' + style + '" data-level="' + level + '" data-splittable="'+splittable+'" data-bg="'+ color +'"></div>';
+        var block = '  <div class="c block" style="' + style + '" data-level="' + level + '" data-splittable="' + splittable + '" data-bg="' + color + '"></div>';
         if (block !== undefined) {
             b += block;
         }
 
     }
-    console.log('b ' + b)
     return b;
 };
 
 // set the width and height of splittable blocks
 setSides = function() {
-    $('.c[data-splittable]').each( function(index, e){
+    $('.c[data-splittable]').each(function(index, e) {
         var self = $(this);
-       // var side = ( self.parent().width() / (self.data('level')*1 + 1)); 
-        var side = ( self.parent().width() / 2.01); 
+        // var side = ( self.parent().width() / (self.data('level')*1 + 1)); 
+        var side = (self.parent().width() / 2.01);
         css = {
-           /* 'padding' : side +'px',*/
-            'width':  side +'px',
-            'height':  side +'px',
-            'display' : 'block'
+            /* 'padding' : side +'px',*/
+            'width': side + 'px',
+            'height': side + 'px',
+            'display': 'block'
         };
         self.css(css);
-        console.log( ' level: '+ self.data('level'))
-        console.log('sides done : side ' + side);
+
     })
-    
-    
-    
+
+
+
 
 }
 
 // splits a circle in four circles
 splitCircle = function(obj, level) {
 
-        $(obj)
-                .data('level', 10)
-                .data('splittable', 0)
-                .removeClass('c')
-                .css('background', '')
-                // .append(level)
-                .append(subBlocks(level));
-        init();
+    $(obj)
+            .data('level', 10)
+            .data('splittable', 0)
+            .removeClass('c')
+            .css('background', '')
+            // .append(level)
+            .append(subBlocks(level));
+    init();
 };
 
 // checks the circle is ready to be splitted
@@ -75,20 +71,16 @@ checkCircle = function() {
     var self = $(this);
 
     var level = self.data('level');
-    
+
 
     if (level < levelMax) {
-        console.log('level' + level);
         self
-                //.html(':o____')
-              // .css('background', 'green');
         splitCircle(self, level + 1);
     }
 };
 init = function() {
     setSides();
     $('.c[data-splittable=1]').on('mouseenter', checkCircle);
-
 }
 init()
 
